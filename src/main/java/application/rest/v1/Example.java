965861dc53e39bf686403d153a7a016d20fea355
rkatikal <rkatikal@gmail.com>
2017-09-24 21:46:47 -0700

    third commit

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -19,7 +19,7 @@ public class Example {
     public Response example() {
         List<String> list = new ArrayList<>();
         //return a simple list of strings
-        list.add("Demo from Ravi");
+        list.add("Demo example");
         return Response.ok(list.toString()).build();
     }
 
